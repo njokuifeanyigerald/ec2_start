5ffb1eacd02f1cdc97c881892548d12d5004644d
Ifeanyi Gerald Njoku <njokugeraldchinedum@gmail.com>
2022-10-23 13:18:07 +0100

    2nd commit

--- lambda_function.py
+++ lambda_function.py
@@ -5,4 +5,4 @@ ec2 = boto3.client('ec2', region_name=region)
 
 def lambda_handler(event, context):
     ec2.start_instances(InstanceIds=instances)
-    print('stopped your instances: ' + str(instances))
+    print('started your instances: ' + str(instances))
